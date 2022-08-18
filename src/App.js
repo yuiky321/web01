4a08e07658816371ec59533fded752a99e49f585
hyewon Lee <yuiky321@naver.com>
2022-08-19 00:19:57 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [disalble, setDisalble] = useState(false);
+  const plus = () => {
+    setCount(count + 1);
+  };
+  const minus = () => {
+    setCount(count - 1);
+  };
+  const on_off = () => {
+    setDisalble(!disalble);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3 data-testid="counter">{count}</h3>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount((prev) => prev + 1)}
+          disabled={disalble}
         >
-          Learn React
-        </a>
+          +
+        </button>
+        <button data-testid="minus-button" onClick={minus} disabled={disalble}>
+          -
+        </button>
+        <button
+          data-testid="on/off"
+          style={{ backgeoundcolor: "blue" }}
+          onClick={on_off}
+        >
+          on/off
+        </button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test("마이너스버튼", () => {
+  render(<App />);
+  const buttonElements = screen.getByTestId("minus-button");
+  expect(buttonElements).toHaveTextContent("-");
+});
+
+test("플러스 버튼", () => {
+  render(<App />);
+  const buttonElements = screen.getByTestId("plus-button");
+  expect(buttonElements).toHaveTextContent("+");
 });
